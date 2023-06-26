70683d85f086e32a10e70965e0cc7e2530d92d29
Oleksandr Havrylko <oleksandr.havrylko@medtronic.com>
2023-06-26 09:39:41 -0700

    Added simplified Future initialization with simple anparametrized lambda

--- pipeline/future.h
+++ pipeline/future.h
@@ -8,7 +8,13 @@ namespace Promise {
     namespace Pipeline {
         struct Future : public Link {
             Future(std::function<void(std::shared_ptr<Publisher::Subscriber> const&)> const& body) :
-            _body(body) {}
+                _body(body) {}
+
+            Future(std::function<void(void)> const& body) :
+                Future([body](auto promise) {
+                    body();
+                    promise->template sink<Publisher::Success>();
+                }) {}
 
             std::shared_ptr<Publisher> const commit() override {
                 return std::make_shared<Concrete::Future>(_body);
